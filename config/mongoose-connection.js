c6f3c8c5c7fa9f4fd7d5082db066c16ba964626c
MirFaizan01 <faizanmeer322@gmail.com>
2024-05-23 11:52:14 +0530

    created the models

---
+++ config/mongoose-connection.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/scatch").then(()=>{
+    console.log(" database connected");
+
+}).catch((err)=>{
+    console.log(err);
+})
+module.exports = mongoose.connection;
---
+++ models/owners-model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://")
+
+const ownerSchema = mongoose.Schema({
+    fullname : {
+        type:String,
+        minLength:3,
+        trim:true,
+    },
+    email:String,
+    password:String,
+    products:{
+        type:Array,
+        default:[]
+    },
+    gstin:String,
+    picture:String
+});
+
+module.exports = mongoose.model("owner", ownerSchema);
